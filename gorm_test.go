82180d5f28df74c5d94091ea94d8a48c504c7176
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-10-14 11:02:26 +0700

    susah nyo gorm

--- gorm_test.go
+++ gorm_test.go
@@ -1,16 +1,20 @@
 package belajar_golang_gorm
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func OpenConnection() *gorm.DB {
 	dialect := mysql.Open("root:admin@tcp(localhost:3306)/belajar_golang_gorm?charset=utf8mb4&parseTime=True&loc=Local")
-	db, err := gorm.Open(dialect, &gorm.Config{})
+	db, err := gorm.Open(dialect, &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 	if err != nil {
 		panic(err)
 	}
@@ -107,6 +111,254 @@ func TestCreateUser(t *testing.T) {
 }
 
 func TestBatchInsert(t *testing.T) {
-	var users = []User
-	
+	var users []User
+	for i := 2; i < 10; i++ {
+		users = append(users, User{
+			ID:       strconv.Itoa(i),
+			Password: "password",
+			Name: Name{
+				FirstName:  "John" + strconv.Itoa(i),
+				MiddleName: "Doe" + strconv.Itoa(i),
+				LastName:   "ireng" + strconv.Itoa(i),
+			},
+		})
+	}
+
+	result := db.Create(&users)
+	assert.Nil(t, result.Error)
+	assert.Equal(t, 8, int(result.RowsAffected))
+}
+
+func TestTransactionSuccses(t *testing.T) {
+	err := db.Transaction(func(tx *gorm.DB) error {
+		err := tx.Create(&User{
+			ID:       "10",
+			Password: "password",
+			Name: Name{
+				FirstName:  "John",
+				MiddleName: "Doe",
+				LastName:   "ireng",
+			},
+		}).Error
+		if err != nil {
+			return err
+		}
+		err = tx.Create(&User{
+			ID:       "11",
+			Password: "password",
+			Name: Name{
+				FirstName:  "John",
+				MiddleName: "Doe",
+				LastName:   "ireng",
+			},
+		}).Error
+		if err != nil {
+			return err
+		}
+
+		err = tx.Create(&User{
+			ID:       "12",
+			Password: "password",
+			Name: Name{
+				FirstName:  "John",
+				MiddleName: "Doe",
+				LastName:   "ireng",
+			},
+		}).Error
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	assert.Nil(t, err)
+}
+
+func TestTransactionError(t *testing.T) {
+	err := db.Transaction(func(tx *gorm.DB) error {
+		err := tx.Create(&User{
+			ID:       "13",
+			Password: "password",
+			Name: Name{
+				FirstName:  "John",
+				MiddleName: "Doe",
+				LastName:   "ireng",
+			},
+		}).Error
+		if err != nil {
+			return err
+		}
+
+		err = tx.Create(&User{
+			ID:       "11",
+			Password: "password",
+			Name: Name{
+				FirstName:  "John",
+				MiddleName: "Doe",
+				LastName:   "ireng",
+			},
+		}).Error
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	assert.NotNil(t, err)
+}
+
+func TestManualTransactionSuccses(t *testing.T) {
+	tx := db.Begin()
+	defer tx.Rollback()
+
+	err := tx.Create(&User{
+		ID:       "13",
+		Password: "password",
+		Name: Name{
+			FirstName:  "John",
+			MiddleName: "Doe",
+			LastName:   "ireng",
+		},
+	}).Error
+	assert.Nil(t, err)
+
+	err = tx.Create(&User{
+		ID:       "14",
+		Password: "password",
+		Name: Name{
+			FirstName:  "John",
+			MiddleName: "Doe",
+			LastName:   "ireng",
+		},
+	}).Error
+
+	assert.Nil(t, err)
+
+	if err == nil {
+		tx.Commit()
+	}
+}
+
+func TestManualTransactionError(t *testing.T) {
+	tx := db.Begin()
+	defer tx.Rollback()
+
+	err := tx.Create(&User{
+		ID:       "15",
+		Password: "password",
+		Name: Name{
+			FirstName:  "John",
+			MiddleName: "Doe",
+			LastName:   "ireng",
+		},
+	}).Error
+	assert.Nil(t, err)
+
+	err = tx.Create(&User{
+		ID:       "14",
+		Password: "password",
+		Name: Name{
+			FirstName:  "John",
+			MiddleName: "Doe",
+			LastName:   "ireng",
+		},
+	}).Error
+
+	assert.NotNil(t, err)
+
+	if err == nil {
+		tx.Commit()
+	}
+}
+
+func TestQuerySingleObject(t *testing.T) {
+	user := User{}
+	result := db.First(&user)
+	assert.Nil(t, result.Error)
+	assert.Equal(t, "1", user.ID)
+
+	user = User{}
+	result = db.Last(&user)
+	assert.Nil(t, result.Error)
+	assert.Equal(t, "9", user.ID)
+}
+
+func TestQuerySingleObjectInlineCondition(t *testing.T) {
+	user := User{}
+	result := db.First(&user, "id = ?", "5")
+	assert.Nil(t, result.Error)
+	assert.Equal(t, "5", user.ID)
+}
+
+func TestQueryAllObject(t *testing.T) {
+	var users []User
+	result := db.Find(&users, "id in ?", []string{"1", "2", "3", "4"})
+	assert.Nil(t, result.Error)
+	assert.Equal(t, 4, len(users))
+}
+
+func TestQueryCondition(t *testing.T) {
+	var users []User
+	result := db.Where("first_name like ?", "%Jo%").
+		Where("passsword = ?", "password").Find(&users)
+	assert.Nil(t, result.Error)
+	assert.Equal(t, 13, len(users))
+}
+
+func TestOrOperator(t *testing.T) {
+	var users []User
+	result := db.Where("first_name like ?", "%Jo%").Or("passsword = ?", "password").Find(&users)
+	assert.Nil(t, result.Error)
+	assert.Equal(t, 13, len(users))
+}
+
+func TestNotOperator(t *testing.T) {
+	var users []User
+	result := db.Not("first_name like ?", "%Jo%").Where("passsword = ?", "password").Find(&users)
+	assert.Nil(t, result.Error)
+	assert.Equal(t, 1, len(users))
+}
+
+func TestSelectFields(t *testing.T) {
+	var users []User
+	result := db.Select("id, name").Find(&users)
+	assert.Nil(t, result.Error)
+
+	for _, user := range users {
+		assert.Equal(t, "id", user.ID)
+		assert.Equal(t, "name", user.Name)
+	}
+	assert.Equal(t, 14, len(users))
+}
+
+func TestStructCondition(t *testing.T) {
+	userCondition := User{
+		Name: Name{
+			FirstName:  "John",
+		},
+	}
+
+	var users []User
+	result := db.Where(userCondition).Find(&users)
+	assert.Nil(t, result.Error)
+	assert.Equal(t, 1, len(users))
+}
+
+func TestMapCondition(t *testing.T) {
+	mapCondition := map[string]interface{}{
+		"middle_name": "",
+	}
+
+	var users []User
+	result := db.Where(mapCondition).Find(&users)
+	assert.Nil(t, result.Error)
+	assert.Equal(t, 14, len(users))
+}
+
+func TestOrderLimitOffset(t *testing.T) {
+	var users []User
+	result := db.Order("id asc, first_name asc").Limit(5).Offset(5).Find(&users)
+	assert.Nil(t, result.Error)
+	assert.Equal(t, 5, len(users))
+	assert.Equal(t, "14", users[0].ID)
 }
